b2980f3bc999d02427cc6bb2d354e92b5ce71c84
saisathvik2005 <saisathvik.ayli@gmail.com>
2026-05-06 11:40:38 +0530

    add context folder

--- codecollab/src/components/layout/ProtectedRoute.jsx
+++ codecollab/src/components/layout/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Navigate, useLocation } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
+  import React from 'react'
+  import { Navigate, useLocation } from 'react-router-dom'
+  import { useAuth } from '../context/AuthContext'
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth()
